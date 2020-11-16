70ced128d4bbe2d93da936dc9c450fd5145deff9
ouyang <67744640+ldj-network@users.noreply.github.com>
2020-11-16 17:42:10 +0800

    Update index.php
    new

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>PHP Store</title>
-  </head>
-  <body>
-    <?php
-      $name = "PHP Store";
-      $credit = 1000; 
+<！DOCTYPE html> 
+<html> 
+  <head> 
+    <title> PHP  Store </ title> 
+  </ head> 
+  <body> <？php $ name = “ PHP Store” ;
+      $信用= 1000 ; 回声“ <h1>欢迎来到”。$名称。“！</ h1>” ;
+      回声“ <h2>您有$”。$信贷。“在您的钱包里。</ h2>”；$产品[[计算机] ] = 750 ;
+	    $
+    
+      
 
-      echo "<h1>Welcome to ".$name."!</h1>";
-      echo "<h2>You have $".$credit." in your wallet.</h2>";
-    ?>
-  </body>
-</html>
+        
+
+	    产品[ 'Car' ] = 15000；
+	    $产品[ 'iPhone' ] = 1000；
+	    $ product [ 'Toaster' ] = 75 ; echo “ <p>一辆汽车要花$”。$产品[ 'Car' ]。“ </ p>”；
+    ？> </ body > </ html >
